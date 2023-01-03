3e492e220e44d1372ac02b3e0cc99d6e7b603533
salamandercrossing <giulio.zani@gmail.com>
2023-01-03 11:48:34 +0000

    added warning

---
+++ main/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import SearchBar from "./SearchBar";
+
+function App() {
+  // requests a json file from the server
+  // and saves as a variable
+  return <SearchBar />;
+}
+
+export default App;
